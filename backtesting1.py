7d0f6bb8d52d59dc17917be491498d2cbac850b5
seanwongeth <sean.wong.2009@business.smu.edu.sg>
2024-03-02 09:14:08 +0800

    added additional inputs

--- backtesting1.py
+++ backtesting1.py
@@ -10,9 +10,10 @@ print(GOOG)
 class RsiOscillator(Strategy): 
   upper_bound = 70
   lower_bound = 30
+  rsi_window = 14
 
   def init(self):
-    self.rsi = self.I(talib.RSI, self.data.Close, 14)
+    self.rsi = self.I(talib.RSI, self.data.Close, self.rsi_window)
 
   def next(self):
     if crossover(self.rsi, self.upper_bound): 
@@ -20,4 +21,9 @@ class RsiOscillator(Strategy):
     elif crossover(self.lower_bound, self.rsi):
       self.buy()
 
-bt = Backtest()
+bt = Backtest(GOOG, RsiOscillator, cash = 10_000)
+
+stats = bt.optimize(upper_bound = range(50, 85, 5), lower_bound = range(10, 45, 5), rsi_window = range(10,30, 2), maximize = 'Sharpe Ratio')
+print(stats)
+
+bt.plot()
